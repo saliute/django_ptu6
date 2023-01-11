cf8f0237a210d1b6125532f84c33e8b1dbea4a9d
Salomeja Marc <salomeja.marciukonyte@gmail.com>
2023-01-11 16:17:30 +0200

    pakeistas views.py failas

--- mysite/library/views.py
+++ mysite/library/views.py
@@ -3,5 +3,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Labas, pasauli!")
+def index2(request):
+    return HttpResponse("Labas, pasauli!")
 
 # Create your views here.
